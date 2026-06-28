991e9aa079725977940e2220fd182cb9b0d47302
NIirbhay <nirbhayruhela0001@gmail.com>
2026-06-28 21:07:30 +0530

    # 1

---
+++ Medium/570_Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,8 @@
+# solution to find managers with at least five direct reports.
+
+SELECT e1.name FROM Employee e1
+INNER JOIN Employee e2
+ON e1.id=e2.managerId
+GROUP BY e2.managerId
+HAVING COUNT(e2.managerId)>=5;
+
