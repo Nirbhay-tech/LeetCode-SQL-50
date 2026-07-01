e3795a16c0e89acca87398a464a056226f46c093
NIirbhay <nirbhayruhela0001@gmail.com>
2026-07-01 18:25:23 +0530

    # 3

---
+++ Medium/1193_Monthly Transactions I.sql
@@ -0,0 +1,10 @@
+# an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount. 
+
+SELECT DATE_FORMAT(trans_date, '%Y-%m' ) AS month,
+country,
+COUNT(id) AS trans_count, 
+SUM(state='approved') AS approved_count, 
+SUM(amount) AS trans_total_amount,
+SUM(IF(state='approved',amount,0)) AS approved_total_amount 
+FROM Transactions
+GROUP BY month, country;
