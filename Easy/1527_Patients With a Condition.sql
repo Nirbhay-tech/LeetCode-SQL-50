18a68797bdc7b4835c864520f4d1618f88891d73
NIirbhay <nirbhayruhela0001@gmail.com>
2026-07-16 12:55:23 +0530

    # 28

---
+++ Easy/1527_Patients With a Condition.sql
@@ -0,0 +1,6 @@
+# solution to find the patient_id, patient_name, and conditions of the patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix.
+
+SELECT patient_id, patient_name, conditions
+FROM Patients
+WHERE conditions REGEXP '(^| )DIAB1';
+
