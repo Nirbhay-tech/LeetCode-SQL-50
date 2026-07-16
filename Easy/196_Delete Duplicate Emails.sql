88699ddd6312142f5956986b910a382d57a8a0d8
NIirbhay <nirbhayruhela0001@gmail.com>
2026-07-16 12:57:23 +0530

    # 29

---
+++ Easy/196_Delete Duplicate Emails.sql
@@ -0,0 +1,9 @@
+#  solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+
+-- For SQL users, please note that you are supposed to write a DELETE statement and not a SELECT one.
+
+DELETE p1
+FROM Person p1 
+INNER JOIN Person p2
+WHERE p1.email = p2.email AND
+p1.id > p2.id;
