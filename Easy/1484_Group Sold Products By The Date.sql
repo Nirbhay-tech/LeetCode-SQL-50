d7dfbcf36335c57f4e5fbaec1150a5be8501e89b
NIirbhay <nirbhayruhela0001@gmail.com>
2026-07-17 15:46:07 +0530

    # 30

---
+++ Easy/1484_Group Sold Products By The Date.sql
@@ -0,0 +1,13 @@
+# solution to find for each date the number of different products sold and their names.
+
+-- The sold products names for each date should be sorted lexicographically.
+
+-- Return the result table ordered by sell_date.
+
+SELECT sell_date, COUNT(DIStINCT product) AS num_sold,
+GROUP_CONCAT(DISTINCT product ORDER BY product) AS products
+FROM Activities 
+GROUP BY 1;
+
+-- GROUP BY sell_date
+-- ORDER BY sell_date;
